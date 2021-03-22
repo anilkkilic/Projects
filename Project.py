b2c27beec728375d9fefb2258415591ccb9f9854
Anıl Kılıç <45760069+anilkkilic@users.noreply.github.com>
2021-03-22 13:28:09 +0300

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,206 +1,163 @@
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-from multiprocessing import Process
-import sys
-import math
+import pandas as pd
+import sklearn.metrics as metrics
+from sklearn import svm
+from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift
+from sklearn.datasets import load_wine
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.metrics.cluster import completeness_score
+from sklearn.metrics.cluster import homogeneity_score
+from sklearn.metrics.cluster import v_measure_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
-rocket = 0
-lisans_num_inputs = []
-ad_soyad_inputs = []
-elo_inputs = []
-ukd_inputs = []
-tur_sayisi_ = 0
-bas_no = len(ad_soyad_inputs)
-masa_no = math.ceil(bas_no/2)
-puan = 0
+# print(dir(datasets))
+wine = load_wine()
+# print(wine.DESCR)
 
+print(wine.feature_names)
+# print(wine.data)
+print(wine.data.shape)
+print(wine.target_names)
+df = pd.DataFrame(wine.data)
+df.columns = wine['feature_names']
+print(df)
 
-def lisans_no():
-    global rocket
-    while rocket < sys.maxsize:
-        while True:
-            lisans_num = input("Oyuncunun lisans numarasini giriniz (bitirmek için 0 ya da negatif giriniz): ")
-            if not lisans_num.isnumeric():
-                print("Sadece sayı giriniz.")
-                continue
-            elif int(lisans_num) <= 0:
-                baslangic_frame()
-            else:
-                lisans_num_inputs.append(lisans_num)
-                break
-        break
-    rocket += 1
-    return isim_soyisim()
+X = wine['data']
+y = wine['target']
 
+# print(X.shape)
+# print(y.shape)
 
-def isim_soyisim():
-    global rocket
-    while rocket < sys.maxsize:
-        while True:
-            ad_soyad = input("Oyuncunun adini-soyadini giriniz:").upper()
-            alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ "
-            special_characters = "!@#$%^&*()-+?_=,<>/"
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.3)
 
-            if any(a in special_characters for a in ad_soyad):
-                print("Özel karakterler kullanılamaz.")
-                continue
-            if not all(a in alphabet for a in ad_soyad):
-                print("Sadece Türkçe harfler kullanınız.")
-                continue
-            else:
-                ad_soyad_inputs.append(ad_soyad)
-                break
-        break
-    rocket += 1
-    return elo()
+# --------------------------K-Means--------------------------------
+kmeans = KMeans(n_clusters=3)
+K_model = kmeans.fit(wine.data)
+# print(K_model)
 
+labels_knn = K_model.labels_
+predict_1 = K_model.fit_predict(wine.data)
+# print(labels_knn)
+# print(predict_1)
 
-def elo():
-    global rocket
-    while rocket < sys.maxsize:
-        while True:
-            elo_ = input("Oyuncunun ELO’sunu giriniz (en az 1000, yoksa 0):")
-            if int(elo_) < 1000:
-                continue
-            else:
-                elo_inputs.append(elo_)
-                break
-        break
-    rocket += 1
-    return ukd()
+centroids = K_model.cluster_centers_
+# print(centroids)
+# 1st, 2nd, 3rd cluster's centroids.
 
+df['target'] = wine['target']
+df['K-Means predicted'] = predict_1
+df = df.loc[:, ['target', 'K-Means predicted']]
+print(df.head(20))
 
-def ukd():
-    global rocket
-    while rocket < sys.maxsize:
-        while True:
-            ukd_ = input("Oyuncunun UKD’sini giriniz (en az 1000, yoksa 0):")
-            if int(ukd_) < 1000:
-                continue
-            else:
-                ukd_inputs.append(ukd_)
-                break
-        break
-    rocket += 1
-    return lisans_no()
+# Adjusted Rand Score
+print("Adjusted Rand Score of K-Means", adjusted_rand_score(df['target'], predict_1))
 
+# Homogeneity Score
+print("Homogeneity Score of K-Means", homogeneity_score(df['target'], predict_1))
 
-def baslangic_frame():
-    baslangic_data = {
-        'LNo': lisans_num_inputs,
-        'Ad-Soyad': ad_soyad_inputs,
-        'ELO': elo_inputs,
-        'UKD': ukd_inputs
-    }
-    baslangic_df = pd.DataFrame(baslangic_data)
-    baslangic_df['BSNo'] = np.arange(1, len(baslangic_df) + 1)
-    baslangic_df.set_index('BSNo', inplace=True)
-    baslangic_df_sorted = baslangic_df.sort_values(by='LNo', ascending=True)
-    baslangic_df_sorted_ = baslangic_df_sorted.sort_values(by='Ad-Soyad')
-    baslangic_df_sorted_f = baslangic_df_sorted_.sort_values(by='ELO', ascending=False)
-    baslangic_df_sorted_final = baslangic_df_sorted_f.sort_values(by='UKD', ascending=False)
-    print(baslangic_df_sorted_final)
-    return tur_sayisi()
+# Completeness Score
+print("Completeness Score of K-Means", completeness_score(df['target'], predict_1))
+
+# V-Measure Score
+print("V-Measure Score of K-Means", v_measure_score(df['target'], df['K-Means predicted']))
+
+# -------------------Mean Shift---------------------
+
+ms = MeanShift()
+ms.fit(wine.data)
+# print(ms.fit(wine.data))
+labels_mean = ms.labels_
+# print(labels_mean)
+predict_2 = ms.fit_predict(wine.data)
+cluster_centers = ms.cluster_centers_
+# print(cluster_centers)
+
+# Adjusted Rand Score
+print("Adjusted Rand Score of Mean Shift:", adjusted_rand_score(df['target'], predict_2))
+
+# Homogeneity Score
+print("Homogeneity Score of Mean Shift:", homogeneity_score(df['target'], predict_2))
+
+# Completeness Score
+print("Completeness Score of Mean Shift:", completeness_score(df['target'], predict_2))
+
+# V-Measure Score
+print("V-Measure Score of Mean Shift", v_measure_score(df['target'], predict_2))
+
+n_clusters_ = len(np.unique(df['target']))
+colors = 10 * ['r.', 'g.', 'b.', 'c.', 'k.', 'y.', 'm.']
+for i in range(len(wine.data)):
+    plt.plot(X[i][0], X[i][1], colors[labels_mean[i]], markersize=3)
+plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], marker=".", color='k', s=20, linewidths=5, zorder=10)
+plt.show()
 
 
-def tur_sayisi():
-    global rocket
-    while rocket < sys.maxsize:
-        while True:
-            tur = input("Turnuvadaki tur sayisini giriniz:")
-            oyuncu_sayisi = math.log(len(ad_soyad_inputs), 2)
-            yukari = math.ceil(oyuncu_sayisi)
-            if int(tur) < yukari or int(tur) > (len(ad_soyad_inputs)-1):
-                print("Tur sayısı bu olamaz:")
-                continue
-            else:
-                tur = tur_sayisi_
-                break
-        break
-    rocket += 1
-    return renk()
+# -------------------------K-Nearest Neighbors (KNN)-------------------------
 
+knn = KNeighborsClassifier(n_neighbors=5)
+# print(knn)
+knn.fit(X_train, y_train)
+# print(knn.fit(X_train, y_train))
+y_pred_1 = knn.predict(X_test)
+print("Accuracy of the KNN is : ", metrics.accuracy_score(y_test, y_pred_1))
 
-def renk():
-    global rocket
-    while rocket < sys.maxsize:
-        while True:
-            baslangic_renk = input("Baslangic siralamasina gore ilk oyuncunun ilk turdaki rengini giriniz (b/s):")
-            if baslangic_renk != 's':
-                print("BSNo'su tek olanlar siyah olmalıdır.")
-                continue
-            else:
-                tur_1_eslestirme_frame()
-                break
-        break
-    rocket += 1
+# K-Fold Cross Validation (K=10)
+scores_knn = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
+print("K-Fold Cross Validation Score of KNN:", scores_knn)
 
+# balanced_accuracy_score
+print("Balanced Accuracy Score of KNN:", balanced_accuracy_score(y_test, y_pred_1))
 
-def tur_1_eslestirme_frame():
-    tur1_data = {
-        'LNo': lisans_num_inputs,
-        '-': None,
-        'Puan': 0.00,
-    }
-    tur1_df = pd.DataFrame(tur1_data)
+# f1-Score
+print("F-1 Score of KNN:", f1_score(y_test, y_pred_1, average='weighted'))
 
-    tur1_df['MNo'] = np.arange(1, len(tur1_df) + 1)
-    tur1_df.set_index('MNo', inplace=True)
-    tur1_df['BNo'] = bas_no
-    beyaz_lno = bas_no % 2 == 0
-    siyah_lno = bas_no % 2 == 1
-    tur1_df.loc[:, 'Siyahlar LNo'] = siyah_lno
-    tur1_df.loc[:, 'Beyazlar LNo'] = beyaz_lno
-    print(tur1_df)
-    return tur_1()
+# ROC AUC Score
+y_pred_proba_1 = knn.predict_proba(X_test)
+print("ROC AUC Score of KNN:", roc_auc_score(y_test, y_pred_proba_1, multi_class='ovr'))
 
+# ROC Curve
+knn_fpr, knn_tpr, _ = metrics.roc_curve(y_test, y_pred_1, pos_label=2)
 
-def tur_1():
-    global rocket
-    while rocket < sys.maxsize:
-        while True:
-            for i in range(0, masa_no):
-                mac_sonucu = input('1. turda' + i + '. masada oynanan macin sonucunu giriniz (0-5):')
-                if int(mac_sonucu) < 0 or int(mac_sonucu) > 5:
-                    continue
-                elif int(mac_sonucu) == 0:
-                    print('Beraberlik')
-                elif int(mac_sonucu) == 1:
-                    print('Beyaz galip')
-                elif int(mac_sonucu) == 2:
-                    print('Siyah galip')
-                elif int(mac_sonucu) == 3:
-                    print('Siyah maça gelmemiş')
-                elif int(mac_sonucu) == 4:
-                    print('Beyaz maça gelmemiş')
-                elif int(mac_sonucu) == 5:
-                    print('Her iki oyuncu da maça gelmemiş')
-            else:
-                break
-        break
-    rocket += 1
+# -------------------------SVM (Support Vector Machine)-----------------
 
+clf = svm.SVC(kernel='linear', probability=True)
+# print(clf)
 
-if __name__ == '__main__':
-    p1 = Process(target=lisans_no())
-    p1.start()
-    p2 = Process(target=isim_soyisim())
-    p2.start()
-    p3 = Process(target=elo())
-    p3.start()
-    p4 = Process(target=ukd())
-    p4.start()
-    p5 = Process(target=renk())
-    p5.start()
-    p6 = Process(target=tur_sayisi())
-    p6.start()
-    p7 = Process(target=tur_1())
-    p7.start()
+clf.fit(X_train, y_train)
+y_pred_2 = clf.predict(X_test)
+print("Accuracy of the SVM is : ", metrics.accuracy_score(y_test, y_pred_2))
 
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    p7.join()
+print(classification_report(y_test, y_pred_2))
+
+# K-Fold Cross Validation (K=10)
+scores_svm = cross_val_score(clf, X, y, cv=10, scoring='accuracy')
+print("K-Fold Cross Validation Scores of SVM:", scores_svm)
+
+# balanced_accuracy_score
+print("Balanced Accuracy Score of SVM", balanced_accuracy_score(y_test, y_pred_2))
+
+# f1-Score
+print("F-1 Score of SVM", f1_score(y_test, y_pred_2, average='weighted'))
+
+# ROC AUC Score
+y_pred_proba_2 = clf.predict_proba(X_test)
+print("ROC AUC Score of SVM:", roc_auc_score(y_test, y_pred_proba_2, multi_class='ovr'))
+
+# ROC Curve
+svm_fpr, svm_tpr, _ = metrics.roc_curve(y_test, y_pred_2, pos_label=2)
+
+plt.plot(knn_fpr, knn_tpr, marker='.', label='KNN')
+plt.plot(svm_fpr, svm_tpr, marker='*', label='SVM')
+plt.title('ROC Curve')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.legend()
+plt.show()
